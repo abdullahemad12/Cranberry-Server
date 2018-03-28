f9556a2afb1a2b0edbe28ab8cce9b381f296088d
Abdullah Emad <abdullahem1997@hotmail.com>
2018-03-28 10:40:00 +0200

    Fix bugs

--- src/lib/Response.java
+++ src/lib/Response.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 import exceptions.NotFoundException;

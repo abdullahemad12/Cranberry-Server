3ff25fcd0aba6b414fd239c1fa717f1cc194fcbb
unknown <almoghazy1@outlook.com>
2018-03-28 01:40:01 -0700

    Bug fixes1

--- src/lib/Response.java
+++ src/lib/Response.java
@@ -85,7 +85,7 @@ public class Response {
 		for (int i = 0; i < buffer.length; i++) {
 			if ( i < tmpByte.length )
 			buffer[i] = tmpByte[i];
-			else buffer[i] = rawBuffer[i] ;
+			else buffer[i] = rawBuffer[i-(tmpByte.length)] ;
 		}
 	}
 
